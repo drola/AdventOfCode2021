3f10d189ffa1cac940d88c5c6a6cf11abf37248f
Matjaž Drolc <mdrolc@gmail.com>
2021-12-12 11:58:38 +0100

    add utility scripts: add_day.sh, run_day.sh, test_day.sh and reorganize repository

--- src/bin/day_1_sonar_sweep.rs
+++ src/bin/day_1_sonar_sweep.rs
@@ -1,3 +1,5 @@
+/// Solution to an Advent of Code problem, day 1, 2021
+/// https://adventofcode.com/2021/day/1
 use std::env;
 use std::fs;
 
@@ -7,11 +9,33 @@ fn count_increases<I: Iterator<Item = i64> + Clone>(it: I) -> u64 {
         .fold(0, |count, (n, next)| count + if next > n { 1 } else { 0 })
 }
 
+fn part_1_times_water_got_deeper<I: std::iter::Iterator<Item = i64> + Clone>(numbers: I) -> u64 {
+    count_increases(numbers)
+}
+
+fn part_2_times_water_got_deeper_windowed<I: std::iter::Iterator<Item = i64> + Clone>(
+    numbers: I,
+) -> u64 {
+    let windows = numbers
+        .clone()
+        .zip(numbers.clone().skip(1))
+        .zip(numbers.skip(2));
+    let window_sums = windows.map(|((a, b), c)| a + b + c);
+
+    count_increases(window_sums)
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
     let contents = fs::read_to_string(filename).expect("Cannot read file");
     let numbers = contents.lines().map(|v| v.parse::<i64>().unwrap());
-    let count = count_increases(numbers);
-    println!("Times water got deeper: {}", count);
+    println!(
+        "[part 1] Times water got deeper: {}",
+        part_1_times_water_got_deeper(numbers.clone())
+    );
+    println!(
+        "[part 2] Times water got deeper (averaged depths): {}",
+        part_2_times_water_got_deeper_windowed(numbers)
+    );
 }
--- src/bin/day_2_dive.rs
+++ src/bin/day_2_dive.rs
@@ -3,30 +3,33 @@ use std::fs;
 use std::num::ParseIntError;
 use std::str::FromStr;
 
-struct Move {
-	dx: i64,
-	ddepth: i64,
+enum Move {
+	Up(i64),
+	Down(i64),
+	Forward(i64),
 }
 
+#[derive(Debug)]
+struct ParseMoveError {}
+
 impl FromStr for Move {
-	type Err = ParseIntError;
+	type Err = ParseMoveError;
+
 	fn from_str(s: &str) -> Result<Self, Self::Err> {
 		let command_and_arg: Vec<&str> = s.split(" ").collect();
 		let command = command_and_arg[0];
-		let arg: i64 = command_and_arg[1].parse()?;
-		match command {
-			"up" => Ok(Move {
-				dx: 0,
-				ddepth: -arg,
-			}),
-			"down" => Ok(Move { dx: 0, ddepth: arg }),
-			"forward" => Ok(Move { dx: arg, ddepth: 0 }),
-			_ => Ok(Move { dx: 0, ddepth: 0 }),
+		let arg: Result<i64, ParseIntError> = command_and_arg[1].parse();
+		match (command, arg) {
+			("up", Ok(arg)) => Ok(Move::Up(arg)),
+			("down", Ok(arg)) => Ok(Move::Down(arg)),
+			("forward", Ok(arg)) => Ok(Move::Forward(arg)),
+			_ => Result::Err(ParseMoveError {}),
 		}
 	}
 }
 
 struct Position {
+	aim: i64,
 	x: i64,
 	depth: i64,
 }
@@ -36,14 +39,64 @@ fn main() {
 	let filename = &args[1];
 	let contents = fs::read_to_string(filename).expect("Cannot read file");
 	let commands = contents.lines().map(|l| l.parse::<Move>().unwrap());
-	let resulting_position =
-		commands.fold(Position { x: 0, depth: 0 }, |position, move_| Position {
-			x: position.x + move_.dx,
-			depth: position.depth + move_.ddepth,
-		});
+
+	let resulting_position = commands.clone().fold(
+		Position {
+			aim: 0,
+			x: 0,
+			depth: 0,
+		},
+		|position, move_| match move_ {
+			Move::Up(v) => Position {
+				x: position.x,
+				depth: position.depth - v,
+				aim: 0,
+			},
+			Move::Down(v) => Position {
+				x: position.x,
+				depth: position.depth + v,
+				aim: 0,
+			},
+			Move::Forward(v) => Position {
+				x: position.x + v,
+				depth: position.depth,
+				aim: 0,
+			},
+		},
+	);
 
 	println!(
-		"x * depth = {}",
+		"[part 1 - without aiming] x * depth = {}",
+		resulting_position.x * resulting_position.depth
+	);
+
+	let resulting_position = commands.fold(
+		Position {
+			aim: 0,
+			x: 0,
+			depth: 0,
+		},
+		|position, move_| match move_ {
+			Move::Up(v) => Position {
+				aim: position.aim - v,
+				x: position.x,
+				depth: position.depth,
+			},
+			Move::Down(v) => Position {
+				aim: position.aim + v,
+				x: position.x,
+				depth: position.depth,
+			},
+			Move::Forward(v) => Position {
+				aim: position.aim,
+				x: position.x + v,
+				depth: position.depth + v * position.aim,
+			},
+		},
+	);
+
+	println!(
+		"[part 2 - with aiming] x * depth = {}",
 		resulting_position.x * resulting_position.depth
 	);
 }
